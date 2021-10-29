6f2d3ec85b0dd266910b915a06933cfba2e91ae6
Norton <PNorton@hartreepartners.com>
2021-10-29 13:43:48 +0100

    first commit]

---
+++ constants.py
@@ -0,0 +1,9 @@
+path = r'C:\Temp'
+file_for_mosaic_data = 'eia-weekly.pkl'
+file_for_metadata = 'eia-weekly-metadata.pkl'
+file_for_scrape = 'eia-weekly-scrape.pkl '
+
+file_for_norm_data = 'eia-weekly-norm-data '
+file_for_pivot_data = 'eia-weekly-pivot-data '
+
+SOURCE_KEY = 'Sourcekey'
